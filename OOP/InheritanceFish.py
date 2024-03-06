8c6566a9324e1b0e23420f2e44d26f3b605419e9
PenguDance <113417079+PenguDance@users.noreply.github.com>
2024-03-06 08:53:56 +0100

    Comments

--- OOP/InheritanceFish.py
+++ OOP/InheritanceFish.py
@@ -38,79 +38,103 @@ class ClownFish(Fish):
         self.avgSharks = [0,0]
         self.nSharks = 0
         self.food = [0,0]
-        for i in range(len(cFishes)):
-            if cFishes[i] == self:
+        for i in range(len(cFishes)): # Går gennem alle fisk
+            if cFishes[i] == self: # Hopper fisken over hvis det er den selv
                 continue
-            dx = cFishes[i].pos[0]-self.pos[0]
+            dx = cFishes[i].pos[0]-self.pos[0] 
             dy = cFishes[i].pos[1]-self.pos[1]
             dc = [dx,dy]
+            # Angiver x- og y-afstande  mellem fiskene
             dist = np.sqrt(dx**2 + dy**2)
-            if dist > self.vis:
+            # Angiver den samlede afstand mellem fiskene
+            if dist > self.vis: # Hvis afstanden mellem fiskene er længere end fiskens synslængde sprænges fisken over
                 continue
 
-            if dist > self.near:
+            if dist > self.near: # Tjekker om den anden fisk er uden for fiskens personligezone
                 for j in range(2):
                     self.avgFish[j] += dc[j]
-            if dist <= self.near:
+                    # Lægger x- og y-afstandene til en liste, der indeholder alle x- og y-afstande
+            if dist <= self.near: # Hvis fisken er indenfor den anden fisks personlige zone
                 for j in range(2):
                     self.avgFish[j] -= 5*dc[j]
-            self.nFish += 1
-        if self.nFish != 0:
+                    # Trækker 5*gange x- og y-afstande fra de samlagte afstande
+
+            self.nFish += 1 # Tæller op hvor mange fisk der er i nærheden
+        if self.nFish != 0: # Hvis der er en eller flere fisk i nærheden
             for i in range(2):
                 self.avgFish[i] = self.avgFish[i]/self.nFish    
+                # Tager gennemsnittet af x- og y-afstandende mellem fisken og de andre fisk
             dist = np.sqrt(self.avgFish[0]**2 + self.avgFish[1]**2)
+            # Finde den gennemsnitlige afstand
             for i in range(2):
                 self.avgFish[i] = self.avgFish[i]/dist
+            # Normaliserer x- og y-afstanden
 
-        for i in range(len(sharks)):
+        for i in range(len(sharks)): # Går gennem alle hajer
             dx = sharks[i].pos[0]-self.pos[0]
             dy = sharks[i].pos[1]-self.pos[1]
             dc = [dx,dy]
             dist = np.sqrt(dx**2 + dy**2)
-            if dist-sharks[i].size > self.vis:
+            # Finder x- og y-afstanden fra fisken til en haj, samt den samlede afstand
+            if dist-sharks[i].size > self.vis: # Tjekker om fisken er udenfor det synlige område
                 continue
             for j in range(2):
                 self.avgSharks[j] += dc[j]
+                # Lægger x- og y-afstande til en samlet liste
             self.nSharks += 1
-        if self.nSharks != 0:
+            # Tæller antallet af hajer i nærheden
+        if self.nSharks != 0: # Hvis der er en eller flere hajer i nærheden
             for i in range(2):
                 self.avgSharks[i] = self.avgSharks[i]/self.nSharks
+                # Tager gennemsnittet af hajernes x- og y-afstande
             dist = np.sqrt(self.avgSharks[0]**2 + self.avgSharks[1]**2)
+            # Finder den gennemsnitlige afstand
             for i in range(2):
                 self.avgSharks[i] = self.avgSharks[i]/dist
+                # Normaliserer x- og y-afstanden
             
             
-        if len(food) > 0:
+        if len(food) > 0: # Tjekker om der er mad
             for i in range(len(food)):
                 dx = food[i].pos[0]-self.pos[0]
                 dy = food[i].pos[1]-self.pos[1]
                 dist = np.sqrt(dx**2 + dy**2)
-                if dist > 2*self.vis:
+                # Finder x- og y afstanden til maden, samt den samlede afstand
+                if dist > 2*self.vis: # Hvis maden er udenfor den dobbelte fiskesynslængde
                     continue
-                if self.food == [0,0]:
-                    self.food = [dy,dx]
+                if self.food == [0,0]: # Hvis der er ikke er valgt mad endnu
+                    self.food = [dy,dx] # Angiver nærmeste mads x- og y-afstand
                     continue
                 if dist < np.sqrt(self.food[0]**2 + self.food[1]**2):
+                    # Tjekker om det nye mad er tættere på end den tidligere som var tættest på
                     for j in range(2):
                         self.food[j] = food[i].pos[j]-self.pos[j]
+                        # Angiver det madens x- og y-afstand
             dist = np.sqrt(self.food[0]**2 + self.food[1]**2)
-            if dist != 0:
+            # Finder den samlede afstand til nærmeste mad
+            if dist != 0: # Hvis der er mad i nærheden
                 for i in range(2):
                     self.food[i] = self.food[i]/dist
+                    # Normaliserer x- og y-afstandene
         for j in range(2):
             speed[j] = (self.avgFish[j] - self.avgSharks[j]*2 + 3*self.food[j])
-        if speed != [0,0]:
+            # Lægger de 3 x- og y-afstande sammen i forholdet, 1:-2:3
+        if speed != [0,0]:  # Hvis der er noget i nærheden
             for j in  range(2):
                 speed[j] = speed[j]/np.sqrt(speed[0]**2 + speed[1]**2)
+                # Normaliserer de nye x- og y-afstande
         for i in range(2):
             self.vel[i] = self.vel[i] + speed[i]*5
+            # Lægger 5 gange den fundne hastighed til fiskens hastighed 
     def checkCol(self,other):
         dist = np.sqrt((self.pos[0]-other.pos[0])**2 + (self.pos[1]-other.pos[1])**2)
-        if dist <= self.size + other.size:
-            if other.name == "Haj":
-                del self
+        # Finder afstanden til det andet objekt
+        if dist <= self.size + other.size: # Hvis afstanden mellem objekterne er mindre end deres samlede størrelser
+            if other.name == "Haj": # Tjekker om det er en haj
+                del self # Sletter fisken
                 return True
             else:
+                # Hvis det ikke er en haj men derimod mad
                 return True
 class Sharks(Fish):
     def __init__(self, pos, len, color, name, size, windowSize, speed, vis, near):
@@ -123,52 +147,66 @@ class Sharks(Fish):
         self.nFish = 0
         self.avgSharks = [0,0]
         self.nSharks = 0
-        for i in range(len(cFishes)):
+        for i in range(len(cFishes)): # Går gennem alle fisk
             dx = cFishes[i].pos[0]-self.pos[0]
             dy = cFishes[i].pos[1]-self.pos[1]
             dc = [dx,dy]
             dist = np.sqrt(dx**2 + dy**2)
-            if dist > self.vis:
+            # Finder x- og y-afstand og den samlede afstand til fisken
+            if dist > self.vis: # Tjekker om fisken er indenfor hajens synslængde
                 continue
             for j in range(2):
-                self.avgFish[j] += 5*dc[j]
-            self.nFish += 1
-        if self.nFish != 0:
+                self.avgFish[j] += 5*dc[j] # Lægger 5 * fiskens x- og y-afstand til en liste
+            self.nFish += 1 # Tæller antallet af fisk i nærheden
+        if self.nFish != 0: # Hvis der er 1 eller flere fisk i nærheden
             for i in range(2):
-                self.avgFish[i] = self.avgFish[i]/self.nFish    
+                self.avgFish[i] = self.avgFish[i]/self.nFish   
+                # Finder gennemsnittet af x- og y-afstanden 
             dist = np.sqrt(self.avgFish[0]**2 + self.avgFish[1]**2)
+            # Finde den gennemsnitlige afstand
             for i in range(2):
                 self.avgFish[i] = self.avgFish[i]/dist
+                # Normaliserer x- og y-afstandene
 
-        for i in range(len(sharks)):
-            if sharks[i] == self:
+        for i in range(len(sharks)): # Går gennem alle hajer
+            if sharks[i] == self: # Tjekker om hajen er sig selv
                 continue
             dx = sharks[i].pos[0]-self.pos[0]
             dy = sharks[i].pos[1]-self.pos[1]
             dc = [dx,dy]
             dist = np.sqrt(dx**2 + dy**2)
-            if dist > self.near:
+            # Finder x- og y-afstanden samt den samlede afstand
+            if dist > self.near: # Hvis hajen er udenfor hajens synslængde
                 continue
             for j in range(2):
                 self.avgSharks[j] += dc[j]
+                # Lægger x- og y-afstanden til en liste
             self.nSharks += 1
-        if self.nSharks != 0:
+            # Tæller antallet af hajer i nærheden
+        if self.nSharks != 0: # Hvis der er 1 eller flere hajer i nærheden
             for i in range(2):
                 self.avgSharks[i] = self.avgSharks[i]/self.nSharks
+                # Finder gennemsnittet af x- og y-afstandene
             dist = np.sqrt(self.avgSharks[0]**2 + self.avgSharks[1]**2)
+            # Finder den genenmsnitlige afstand
             for i in range(2):
                 self.avgSharks[i] = self.avgSharks[i]/dist
+                # Normaliserer x- og y-afstanden
             
         for j in range(2):
-            speed[j] = self.avgFish[j] - self.avgSharks[j]
-            
+            speed[j] = self.avgFish[j] - 2*self.avgSharks[j]
+            # Lægger fiske afstandene og haj afstandene sammen i forholdet 1:-2
 
         for i in range(2):
             self.vel[i] = self.vel[i] + speed[i]*5
+            # Lægger 5 gange den fundne hastighed til hajens egen hastighed 
         
     def eat(self):
         self.size = self.size + 1
+        # Gør hajen større
     def freeze(self):
         self.frozen = True
+        # Angiver at hajen er frosset
     def unfreeze(self):
         self.frozen = False
+        # Angiver at hajen er optøet
--- OOP/main.py
+++ OOP/main.py
@@ -4,41 +4,41 @@ import random as r
 from Library import Vector
 from InheritanceFish import ClownFish,Sharks
 from food import Food
-windowSize = [800,600]
-clock= pygame.time.Clock()
-# Bredde og højde på vinduet
-# Initialisering af Pygame
-pygame.init()
+windowSize = [800,600] # Bredde og højde på vinduet
+pygame.init() # Initialisering af Pygame
+clock = pygame.time.Clock() # Tidssytem til spillet
 fishSpawnEvent = 1
 sharkFreezeEvent = 2
 sharkUnfreezeEvent = 3
 foodSpawnEvent = 4
 hungerEvent = 5
-screen = pygame.display.set_mode((windowSize[0],windowSize[1]))
-pygame.display.set_caption('Flokkesimulation')
-# Font til tekst i vinduet
-font = pygame.font.SysFont("Arial", 36)
-cfish = [0]*100
-sharks = [0]*10
-food = [0]
+# Events til spillet
+screen = pygame.display.set_mode((windowSize[0],windowSize[1])) # Laver kanvas
+pygame.display.set_caption('Flokkesimulation') # Navngiver programnavn
+font = pygame.font.SysFont("Arial", 36) # Font til tekst i vinduet
+cfish = [0]*100 # Start array til fisk
+sharks = [0]*10 # Start array til hajer
+food = [0]      # Start array til mad
 frozenSharks = [0]
-time = clock.get_time()
 def newFish():
     cfish.append(ClownFish([r.randint(0,windowSize[0]),r.randint(0,windowSize[1])],[(r.randint(0,200)-100)/100,(r.randint(0,200)-100)/100],(100,100,100),"Fisk",5,windowSize,5,200,50))
+    # Laver en ny fisk med værdierne ([x,y],[x-has,y-has],(r,g,b),"navn",størrelse, [vindue-x,vindue-y], synslængde, personligzone)
     print(f"Ny fisk :D {len(cfish)}")
 def newShark():
     sharks.append(Sharks([r.randint(0,windowSize[0]),r.randint(0,windowSize[1])],[(r.randint(0,200)-100)/100,(r.randint(0,200)-100)/100],(255,10,50),"Haj",5,windowSize,1.5,300,150))
+     # Laver en ny haj med værdierne ([x,y],[x-has,y-has],(r,g,b),"navn",størrelse, [vindue-x,vindue-y], synslængde, personligzone)
 def newFood():
     if len(food) < 5:
         food.append(Food(windowSize))
+    # Laver mere mad, hvis der er mindre end 5 stykker mad    
 def sharkFreeze():
-    x = r.randint(0,len(sharks)-1)
-    sharks[x].freeze()
-    frozenSharks[0] = sharks[x]
-    pygame.time.set_timer(sharkUnfreezeEvent,1000, loops = 1)
+    x = r.randint(0,len(sharks)-1) # Finder en tilfældig haj
+    sharks[x].freeze() # Fryser hajen
+    frozenSharks[0] = sharks[x] # Angiver frozenSharks[0] som den frozne haj
+    pygame.time.set_timer(sharkUnfreezeEvent,1000, loops = 1) # Starter en timer på 1 sekund
 def sharkUnfreeze():
-    frozenSharks[0].unfreeze()
-    pygame.time.set_timer(sharkFreezeEvent, r.randint(500,4000), loops = 1)
+    frozenSharks[0].unfreeze() # optøer hajen
+    pygame.time.set_timer(sharkFreezeEvent, r.randint(500,4000), loops = 1) # Start en timer til at fryse en ny haj
     
 def setup():
     for i in range(len(cfish)):
@@ -46,49 +46,46 @@ def setup():
     for i in range(len(sharks)):
         sharks[i] = Sharks([r.randint(0,windowSize[0]),r.randint(0,windowSize[1])],[(r.randint(0,200)-100)/100,(r.randint(0,200)-100)/100],(255,10,50),"Haj",5,windowSize,1.5,300,150)
     food[0] = Food(windowSize)
+    # Laver start fisk, hajer og mad
 pygame.time.set_timer(fishSpawnEvent, 1000)
 pygame.time.set_timer(sharkFreezeEvent, r.randint(500,2000), loops = 1)
 pygame.time.set_timer(foodSpawnEvent, 100, loops = 1)
 pygame.time.set_timer(hungerEvent, 5000)
+# Timere til simulationen
 def draw():
-    clock.tick(60)
-    screen.fill((200, 200, 200))
-    for i in range(len(cfish)):
-        if i >= len(cfish):
+    clock.tick(60) # sætter tickrate til 60 tps
+    screen.fill((200, 200, 200)) # Farver skærmen lysegrå
+    for i in range(len(cfish)): # Går gennem alle fisk
+        if i >= len(cfish): # I tilfælde af at en fisk er slettet, sørger det for at programmet ikke crasher
             break
-        if len(food) > 0:
-            for j in range(len(food)):
-                if cfish[i].checkCol(food[j]) == True:
-                    cfish[i].size += 1
-                    food.pop(j)
+        if len(food) > 0: # Tjekker om der er mad
+            for j in range(len(food)): # Går gennem alt maden
+                if cfish[i].checkCol(food[j]) == True: # Tjekker om fisken kolliderer med maden
+                    cfish[i].size += 1 # Gør fisken større
+                    food.pop(j) # Fjerner maden
                     print("Yum")
                     break
         cfish[i].update(cfish,sharks,food)
         cfish[i].show(screen)
-        for j in range(len(sharks)):
-            if cfish[i].checkCol(sharks[j]) == True:
-                cfish.pop(i)
+        # Opdaterer og viser fisken
+        for j in range(len(sharks)): # Går gennem alle hajer
+            if cfish[i].checkCol(sharks[j]) == True: # Tjekker om fisken kolliderer med en haj
+                cfish.pop(i) # Fjerner fisken
                 print("Yum")
-                sharks[j].eat()
+                sharks[j].eat() # Kører hajens spise funktion
                 break
         
-    for i in range(len(sharks)):
-        if sharks[i].frozen == False:
-            sharks[i].update(cfish,sharks,food)
-        sharks[i].show(screen)
-    for i in range(len(food)):
+    for i in range(len(sharks)): # Går gennem alle hajer
+        if sharks[i].frozen == False: # Tjekker om hajen ikke er frosen
+            sharks[i].update(cfish,sharks,food) # Opdaterer hajen
+        sharks[i].show(screen) # Viser hajen
+    for i in range(len(food)): # Går gennem alt mad
         food[i].update()
-        food[i].show(screen)
-        #cfish[i].setVel(cfish)
-#NB! bruger her dependency injection for at undgå circular import dependencies in other files.
-#cfish1.swim()#MyVector(1,1)) #r.randint(-1,1),r.randint(-1,1)))
-#cfish1.show(screen)
-#shark1.swim(cfish1)
-#shark1.show(screen)
-# Vis vindhastighed og retning i vinduet
+        food[i].show(screen) # Opdaterer og viser maden
     text = f"amount of fishes = {len(cfish)}  amount  of sharks = {len(sharks)}"
     text_surface = font.render(text, True, (0, 0, 0))
     screen.blit(text_surface, (10, 50))
+    # Tekst der viser antallet af fisk og hajer
     pygame.display.update()
 setup()
 running = True
